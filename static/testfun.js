0bf675a93c70b88c7ac1758e969dc3c91302afbc
christak12065 <96892778+christak12065@users.noreply.github.com>
2022-01-09 10:54:05 -0500

    Update testfun.js
    updated logic app url

--- static/testfun.js
+++ static/testfun.js
@@ -1,7 +1,7 @@
 function testfun(){
 const date = new Date(Date.now());
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-let x = axios.post('https://webappfinal.azurewebsites.net', {
+let x = axios.post('https://prod-36.eastus.logic.azure.com:443/workflows/5f44f911d28a4a7793dace2e98490d5a', {
     id: self.crypto.randomUUID(),
     month: months[date.getMonth()],
     day: date.getDate(),
