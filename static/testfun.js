0877a7565de445c62354f91a5f48c32aec4da25b
christak12065 <96892778+christak12065@users.noreply.github.com>
2022-01-20 20:07:28 -0500

    Update testfun.js

--- static/testfun.js
+++ static/testfun.js
@@ -1,7 +1,7 @@
 function testfun(){
 const date = new Date(Date.now());
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-let x = axios.post('https://prod-36.eastus.logic.azure.com:443/workflows/5f44f911d28a4a7793dace2e98490d5a', {
+let x = axios.post('https://prod-36.eastus.logic.azure.com:443/workflows/5f44f911d28a4a7793dace2e98490d5a/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=C9-nLkA2Akoa3VQOW7EzKRcvZ4mp1_FPKzdVCazlGbA', {
     id: self.crypto.randomUUID(),
     month: months[date.getMonth()],
     day: date.getDate(),
